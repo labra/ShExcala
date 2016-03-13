e93eb7386d895cb9e442f5612bd06038081896a8
labra <jelabra@gmail.com>
2016-03-13 20:34:47 +0100

    Refactor RDF2SchemaTest

--- src/test/scala/es/weso/shacl/converter/Schema2RDFTest.scala
+++ src/test/scala/es/weso/shacl/converter/Schema2RDFTest.scala
@@ -1,6 +1,5 @@
 package es.weso.shacl.converter
 
-
 import org.scalatest._
 import org.scalatest.prop._
 import com.hp.hpl.jena.rdf.model._
@@ -9,6 +8,8 @@ import java.io.StringReader
 import org.apache.jena.riot.RDFLanguages
 import es.weso.shacl.Schema
 import es.weso.rdf.jena.RDFAsJenaModel
+import es.weso.utils.JenaUtils
+import util._
 
 class Schema2RDFTest extends FunSpecLike
     with Matchers
@@ -16,12 +17,12 @@ class Schema2RDFTest extends FunSpecLike
 
   describe("Schema2RDF") {
 
-      it("Should convert a simple definition") {
-        val schemaStr = """|prefix : <http://example.org/>
+    it("Should convert a simple definition") {
+      val schemaStr = """|prefix : <http://example.org/>
                      |:a { :b IRI }
                      |""".stripMargin
-                     
-        val expectedStr = """|@prefix :      <http://example.org/> .
+
+      val expectedStr = """|@prefix :      <http://example.org/> .
                           |@prefix sh:    <http://www.w3.org/ns/shacl#> .
                           |@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .
                           |
@@ -31,25 +32,31 @@ class Schema2RDFTest extends FunSpecLike
                           |  sh:minCount   1 ;
                           |  sh:nodeKind   sh:IRI ;
                           |  sh:predicate  :b ] .""".stripMargin
-                          
-        val expectedModel = ModelFactory.createDefaultModel;
-        val reader = new StringReader(expectedStr) ;
-        RDFDataMgr.read(expectedModel, reader, null, RDFLanguages.TURTLE) ;
-        
-        val (schema,pm) = Schema.fromString(schemaStr).get
-        val rdf = RDFAsJenaModel.empty
-        Schema2RDF.schema2RDF(schema,rdf)
-        val convertedModel = rdf.model
-        if (convertedModel.isIsomorphicWith(expectedModel)) {
-          println("Are isomorphic")
-        } else {
-          println("Models are not equivalent")
-          println("Converted model")
-          convertedModel.write(System.out, "TURTLE")
-          println("Expected model")
-          expectedModel.write(System.out, "TURTLE")
+
+      val rdf = RDFAsJenaModel(ModelFactory.createDefaultModel())
+      val tryConvert = for {
+        expectedModel <- JenaUtils.parseFromString(expectedStr)
+        (schema, pm) <- Schema.fromString(schemaStr)
+      } yield (expectedModel, schema)
+
+      tryConvert match {
+        case Success((expectedModel, schema)) => {
+          val convertedModel = rdf.model
+          if (convertedModel.isIsomorphicWith(expectedModel)) {
+            println("Are isomorphic")
+          } else {
+            println("Models are not equivalent")
+            println("Converted model")
+            convertedModel.write(System.out, "TURTLE")
+            println("Expected model")
+            expectedModel.write(System.out, "TURTLE")
+          }
+
         }
+        case Failure(e) => fail(s"Error converting $e")
       }
 
     }
+
+  }
 }
